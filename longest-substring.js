485d52baaa7f77b006f771e7eb2b96a732c19cab
Johnny <po_bo@hotmail.com>
2018-02-03 20:14:15 -0800

    finished problem with (0)N solution

--- longest-substring.js
+++ longest-substring.js
@@ -13,77 +13,22 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
  * @return {number}
  */
 
-let str = 'pwwkew'
+let string = 'pwwkew';
 
-// var lengthOfLongestSubstring = function(s) {
-//   if (s.length < 2) {
-//     return s.length;
-//   }
-
-//   let left, right, maxLength = 0;
-//   for (left = 0, right = 1; r < s.length; r++) {
-//             i = s.lastIndexOf(s[r], r-1);
-//         if (i >= 0) {
-//             maxLen = Math.max(maxLen, r - l);
-//             l = Math.max(l, i + 1);
-//         }
-//   }
-//       return Math.max(maxLen, r - l);
-// };
-// var lengthOfLongestSubstring = function(s) {
-//     var maxLen,
-//         l,
-//         r;
-
-//     if (s.length < 2) {
-//         return s.length;
-//     }
-
-//     maxLen = 0;
-
-//     for (l = 0, r = 1; r < s.length; r++) {
-//         k = s.lastIndexOf(s[r], r-1);
-//         if (k >= 0) {
-//             maxLen = Math.max(maxLen, r - l);
-//             l = Math.max(l, k + 1);
-//         }
-//     }
-//     return Math.max(maxLen, r - l);
-// };
-// console.log(lengthOfLongestSubstring(str));
-
-
-const add = (num) => {
-  let test = 1
-  for (i = 0; i < num; i++){
-    total = test + i
+function substring (s) {
+  let start = 0, end;
+  let seen = {};
+  let maxLength = 0;
+  for (end = 0; end < s.length; end++) {
+    if (!seen.hasOwnProperty([s[end]])) {
+      seen[s[end]] = end;
+      maxLength = Math.max(maxLength, end - start + 1);
+    } else {
+      start = Math.max(start, seen[s[end]] + 1);
+      seen[s[end]] = end;
+      maxLength = Math.max(maxLength, end - start + 1);
+    }
   }
-  return total
+  return maxLength;
 }
-aj = 'johnny'
-console.log(aj)
-console.log(add(10))
-
-// var lengthOfLongestSubstring = function(s) {
-//   if (s.length < 2) {
-//     return s.length;
-//   }
-//   let hash = {};
-//   let result = [];
-//   let count = 0;
-//   for (let i = 0; i < s.length; i++) {
-//     for (let j = i; j < s.length; j++) {
-//       let currVal = s[j];
-//       if (!hash[currVal]) {
-//         hash[currVal] = currVal;
-//         count++;
-//       } else {
-//         result.push(count);
-//         count = 0;
-//         hash = {};
-//         break;
-//       }
-//     }
-//   }
-//   return Math.max(...result);
-// };
+// console.log(substring(string));
